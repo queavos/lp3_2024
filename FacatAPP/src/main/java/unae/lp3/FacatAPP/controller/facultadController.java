4731981175a5f776ba749458d090a7933330c3fb
queavos <queavos.dev@gmail.com>
2024-04-16 21:23:31 -0400

    webjars fails

--- FacatAPP/src/main/java/unae/lp3/FacatAPP/controller/facultadController.java
+++ FacatAPP/src/main/java/unae/lp3/FacatAPP/controller/facultadController.java
@@ -8,33 +8,36 @@ import java.util.LinkedList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import unae.lp3.FacatAPP.model.Facultad;
 
 /**
  *
  * @author ossva
  */
-//@RestController
 @Controller
-public class IndexController {
-
-    @GetMapping("/")
-    public String index(Model model) {
-
-        model.addAttribute("datos",
-                "Hola Mundo");
-
-        return "index";
-    }
+@RequestMapping("/facultades")
+public class facultadController {
 
     @GetMapping("/list")
     public String list(Model model) {
         LinkedList<Facultad> facultades = new LinkedList<Facultad>();
-        //facultades.add("FACAT");
-        //facultades.add("FACEM");
-        //facultades.add("FACSA");
-
+        Facultad facu1 = new Facultad();
+        facu1.setNombre("Facultad de Ciencias Artes y Tecnologia");
+        facu1.setSigla("FACAT");
+        Facultad facu2 = new Facultad();
+        facu2.setNombre("Facultad de Ciencias de la Salud");
+        facu2.setSigla("FACSA");
+        Facultad facu3 = new Facultad();
+        facu3.setNombre("Facultad de MIERDA");
+        facu3.setSigla("FACEM");
+        Facultad facu4 = new Facultad();
+        facu4.setNombre("Facultad de Ciencias Veterinarias");
+        facu4.setSigla("FACVA");
+        facultades.add(facu1);
+        facultades.add(facu2);
+        facultades.add(facu3);
+        facultades.add(facu4);
         model.addAttribute("title",
                 "Lista de Faultades");
         model.addAttribute("header",
